90924cf3261650ddb71b70940f3edf835d59cb45
Kélian PONS <kelian.pons@gmail.com>
2026-05-26 20:17:16 +0200

    refactor: class separation

--- src/env/chart.py
+++ src/env/chart.py
@@ -46,7 +46,35 @@ class Chart:
 class BalanceAccount():
     """
     """
-    def __init__(self):
-        ...
+    def __init__(self, initial_balance: float):
+        balance_history = [initial_balance]
+        balance = initial_balance
     
-    
+    def add_profit(self, profit: float):
+        self.balance += profit
+        self.balance_history.append(self.balance)
+
+    def add_loss(self, loss: float):
+        """
+        No very usefull, prefer using add_profit
+        """
+        self.balance -= loss
+        self.balance_history.append(self.balance)
+
+    def display(self):
+        """
+        Display the balance history using plotly
+        """
+        fig = go.Figure()
+        fig.add_trace(go.Scatter(
+            y=self.balance_history,
+            mode='lines',
+            name='Account Balance'
+        ))
+        fig.update_layout(
+            title='Account Balance History',
+            xaxis_title='Steps',
+            yaxis_title='Balance',
+            template='plotly_dark'
+        )
+        fig.show()
